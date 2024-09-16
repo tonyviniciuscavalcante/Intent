449ff819bcd230a96cee01aba57963852a22433b
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-09-16 20:06:30 -0300

    companion object

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/MainActivity.kt
@@ -12,8 +12,17 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    companion object Constantes {
+        const val PARAMETRO_EXTRA = "PARAMETRO_EXTRA"
+        const val PARAMETRO_REQUEST_CODE = 0
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+
+        
+
     }
 }
