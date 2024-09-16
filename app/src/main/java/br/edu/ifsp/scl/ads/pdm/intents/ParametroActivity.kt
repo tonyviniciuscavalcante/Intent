09fcab7e786dd16d20829b0e95830fe4a1a5516b
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-09-16 19:39:20 -0300

    estruturando a parametro xml

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/ParametroActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/ParametroActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.pdm.intents.databinding.ActivityParametroBinding
 
 class ParametroActivity : AppCompatActivity() {
+    private val apb: ActivityParametroBinding by lazy {
+        ActivityParametroBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_parametro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(apb.root)
     }
 }
