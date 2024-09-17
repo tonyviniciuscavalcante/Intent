bd1edec6bf7a2b619fb7d8bbb31ab5526b8fbcab
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-09-16 21:49:58 -0300

    parl = registerForActivityResult

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/MainActivity.kt
@@ -3,6 +3,10 @@ package br.edu.ifsp.scl.ads.pdm.intents
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,9 +19,10 @@ class MainActivity : AppCompatActivity() {
 
     companion object Constantes {
         const val PARAMETRO_EXTRA = "PARAMETRO_EXTRA"
-        const val PARAMETRO_REQUEST_CODE = 0
+        // const val PARAMETRO_REQUEST_CODE = 0
     }
 
+    private lateinit var parl: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +33,19 @@ class MainActivity : AppCompatActivity() {
             title = getString(R.string.app_name)
             subtitle = this@MainActivity.javaClass.simpleName
         }
+
+        parl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            object: ActivityResultCallback<ActivityResult> {
+                override fun onActivityResult(result: ActivityResult) {
+                    if (result.resultCode == RESULT_OK) {
+                        result.data?.getStringExtra(PARAMETRO_EXTRA)?.let {
+                            amb.parametroTv.text = it
+                        }
+                    }
+                }
+            }
+        )
 
         amb.entrarParametroBt.setOnClickListener {
 //            val parametroIntent = Intent(this, ParametroActivity::class.java)
