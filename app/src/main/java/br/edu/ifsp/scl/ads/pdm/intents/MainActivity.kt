8b6ea707105fd4612879062ae89d49f5358ceafb
Tony Vinicius Cavalcante <82682652+tonyviniciuscavalcante@users.noreply.github.com>
2024-09-30 19:47:29 -0300

    Update MainActivity.kt

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/intents/MainActivity.kt
@@ -56,7 +56,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
+        return true
     }
 
 }
